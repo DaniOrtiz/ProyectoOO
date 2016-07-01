a272ab7a3c7f2011633f0b1e0b8b1deb2a2d0038
DaniOrtiz <danidaniortiz@gmail.com>
2016-06-30 21:17:36 -0430

    Listos para mandar (:

--- mod_bfs.rb
+++ mod_bfs.rb
@@ -8,27 +8,29 @@
 
 module BFS
 
+	# Método que recibe un bloque de manera implícita 
+	# e itera desde self haciendo un recorrido BFS.
 	def bfs
 		cola = []
 		cola << self
 		while (!cola.empty?)
 			auxnode = cola.shift
+			yield auxnode
 			auxnode.each do |child|
 				cola << child unless child == nil
 			end
 		end 
 	end
 
-	def recoger(&block)
+	# Método que recibe un predicado (bloque) de manera
+	# explícita, itera desde self haciendo un recorrido 
+	# BFS y en el camino va recogiendo los nodos que 
+	# cumplan con el predicado
+	
+	def recoger(block)	
 		cola = []
-		cola << self
-		while (!cola.empty?)
-			auxnode = cola.shift 
-			auxnode.each do |child|
-				if block.call(child)
-					cola << child unless child == nil
-				end
-			end
-		end
+		self.bfs {|child| cola << child if block.call(child)}	
+		return cola
 	end
+
 end
--- trees.rb
+++ trees.rb
@@ -16,23 +16,23 @@ class Tree
 	include BFS
 	include DFS
 
-	attr_accessor :node # Objeto que guarda el valor del nodo actual
+	attr_accessor :valor # Objeto que guarda el valor del nodo actual
 
 	def mutar(objeto)
-		@node =self.node.mutar(objeto)
+		@valor =self.valor.mutar(objeto)
 	end
 
 end
 
 # Clase árbol binario
-class BinaryTree < Tree
+class ArbolBinario < Tree
 	 
 	attr_reader :left,  # Referencia al hijo izquierdo
 	            :right  # Referencia al hijo derecho
 
 	# Constructor de un árbol binario.
-	def initialize(node, left=nil, right=nil)
-		@node = node 
+	def initialize(valor, left=nil, right=nil)
+		@valor = valor 
 		@left = left 
 		@right = right 
 	end
@@ -49,13 +49,13 @@ class BinaryTree < Tree
 end
 
 # Clase árbol rosa
-class PinkTree < Tree
+class ArbolRosa < Tree
 	 
 	attr_reader :sons   # Arreglo de hijos
 
 	# Constructor de un árbol rosa.
-	def initialize(node, *hijos)
-		@node = node 
+	def initialize(valor, *hijos)
+		@valor = valor
 		@sons = []
 		hijos.each do |elem| # Iteramos sobre los elementos de la lista hijos
 			@sons << elem    # Se agregan a la lista de hijos sons
